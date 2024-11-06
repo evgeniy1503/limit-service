ff39b7863a05bd3563f3b23f537e9bd67424a733
Evgeniy Prokhorov <eprohorov@dev.vtb.ru>
2024-11-06 18:01:51 +0300

    Инициализация проекта

---
+++ src/test/java/ru/prokhorov/limitservice/LimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.prokhorov.limitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
